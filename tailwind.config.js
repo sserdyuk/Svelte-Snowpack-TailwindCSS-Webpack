c4ad039d37933eb2dd7e19c03e0a52e57ada0c45
Sergei Serdyuk <sergei@redleafsoft.com>
2021-02-25 13:39:28 -0500

    Fixed up Tailwind config to keep Svelte's conditional classes at purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,25 @@
 const colors = require('tailwindcss/colors');
 module.exports = {
-  purge: [
-    './public/**/*.html',
-    './src/**/*.svelte',
-    './src/**/*.js',
-    './src/**/*.ts',
-  ],
+  purge: process.env.NODE_ENV === 'production' && {
+    content: ['./public/**/*.html', './src/**/*.svelte', './src/**/*.js', './src/**/*.ts'],
+    options: {
+      whitelistPatterns: [/svelte-/],
+      defaultExtractor: (content) => {
+        const regExp = new RegExp(/[A-Za-z0-9-_:/]+/g);
+        const matchedTokens = [];
+        let match = regExp.exec(content);
+        while (match) {
+          if (match[0].startsWith('class:')) {
+            matchedTokens.push(match[0].substring(6));
+          } else {
+            matchedTokens.push(match[0]);
+          }
+          match = regExp.exec(content);
+        }
+        return matchedTokens;
+      },
+    },
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
